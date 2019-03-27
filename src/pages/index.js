1ec08fc7a0706468ea8707b47907e0d49837b8ed
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2019-03-27 16:24:39 +0900

    スタイル修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 // eslint-disable-next-line
 import React from "react"
-import { jsx, css } from "@emotion/core"
+import { jsx, css, keyframes } from "@emotion/core"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,8 +9,7 @@ import SEO from "../components/seo"
 import image from "src/images/photo/groupPhoto.jpg"
 import logo from "src/images/formationLogo.png"
 
-const wrapper = css`
-`
+const wrapper = css``
 
 const logoStyle = css`
   position: absolute;
@@ -20,25 +19,44 @@ const logoStyle = css`
   top: 3vh;
 `
 
+const bounce = keyframes`
+  from, 20%, 53%, 80%, to {
+    opacity: 0.5;
+  }
+
+  40%, 43% {
+    opacity: 0.6;
+  }
+
+  70% {
+    opacity: 0.3;
+  }
+
+  90% {
+    opacity: 0.6;
+  }
+`
+
+const textStyle = css`
+  animation: ${bounce} 3s ease infinite;
+  position: absolute;
+  color: white;
+  width: 50vw;
+  margin: auto;
+  top: 30vh;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0.6;
+  font-weight: bold;
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <div css={wrapper}>
       <img css={logoStyle} src={logo} alt="" />
-      <p
-        style={{
-          position: "absolute",
-          color: "white",
-          width: "50vw",
-          margin: "auto",
-          top: "35vh",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          opacity: 0.6,
-          fontWeight: "bold",
-        }}
-      >
+      <p css={textStyle}>
         どうしようもないこんな世の中だけど、僕らには音楽がある。友達がいる。もしきみが今すごくつらいなら僕らと一緒にいたらいい。一緒にグッドミュージックで踊ったり踊らなかったりしよう。ただ一緒にいよう。FORMATIONを組もう。
       </p>
       <div>
